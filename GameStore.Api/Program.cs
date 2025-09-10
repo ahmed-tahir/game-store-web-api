6268347375ec596e93e306a888c9dadc491cf746
Tahir Ahmed <tahirat97@gmail.com>
2025-09-10 23:48:49 +0530

    Added packages to support EF core, generate Sqlite db, DbContext class, migration, etc.

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,18 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configures our DbContext with SQLite
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreDbContext>(connectionString);
+
 var app = builder.Build();
 
+// Map endpoints
 app.MapGamesEndpoints();
+
+// Execute migrations on startup
+app.MigrateDB();
 
 app.Run();
